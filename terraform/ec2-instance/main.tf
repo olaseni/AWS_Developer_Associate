9bb81ad8dea3e2b29d9ef4059e917755718dc8f9
olaseni <277661+olaseni@users.noreply.github.com>
2024-11-25 22:24:50 +0000

    Start using links to compose common functionality in files

--- terraform/ec2-instance/main.tf
+++ terraform/ec2-instance/main.tf
@@ -1,21 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "eu-west-2"
-}
-
 resource "aws_instance" "app_server" {
-  ami           = "ami-0e8d228ad90af673b"
-  instance_type = "t2.micro"
+  ami           = var.instance_ami
+  instance_type = var.instance_type
 
   tags = {
     Name = "ExampleAppServerInstance"
